5ccf4689a4fca3d77314eeece0791303720e4502
xy1xy1111 <143323662+xy1xy1111@users.noreply.github.com>
2023-08-27 18:37:43 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
+  appid: "wxc62b19172c8ccca4",
+  appsecret: "21bd80cef458d94be1c0d441c4307af7",
+  template_id: "64KgsHXU3sMnn70NdHkHqIkUqfpE1Spb0GYbJtTi4dA",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
+  city: "巴中",
   first_date: "2022-11-10",
 };
